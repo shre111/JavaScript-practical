07af5ee92d003f447ac10a62dec1df4af36d5508
shre111 <155060758+shre111@users.noreply.github.com>
2025-04-23 22:20:19 +0530

    Update ArrayQustion.js

--- ArrayQustion.js
+++ ArrayQustion.js
@@ -138,3 +138,53 @@ function findSecondLargest(arr) {
 const numbers = [3, 5, 7, 2, 8, 1];
 const secondLargestNumber = findSecondLargest(numbers);
 console.log(`The second largest number is: ${secondLargestNumber}`);
+
+// Question: 10
+const students = [
+  { name: "aryan", subject: "English", age: 20, address: "ahmedabad" },
+  { name: "divya", subject: "Computer", age: 19, address: "kerela" },
+  { name: "aryan", subject: "hindi", age: 18, address: "goa" }
+];
+
+// with for loop
+let resultFor = {};
+
+for (let i = 0; i < students.length; i++) {
+  const { name, subject, age, address } = students[i];
+  if (!resultFor[name]) {
+    resultFor[name] = [];
+  }
+  resultFor[name].push({ subject, age, address });
+}
+
+console.log("For loop result:", resultFor);
+
+
+// Using forEach loop
+let resultForEach = {};
+
+students.forEach(student => {
+  const { name, subject, age, address } = student;
+  if (!resultForEach[name]) {
+    resultForEach[name] = [];
+  }
+  resultForEach[name].push({ subject, age, address });
+});
+
+console.log("forEach result:", resultForEach);
+
+// Using while loop
+
+let resultWhile = {};
+let i = 0;
+
+while (i < students.length) {
+  const { name, subject, age, address } = students[i];
+  if (!resultWhile[name]) {
+    resultWhile[name] = [];
+  }
+  resultWhile[name].push({ subject, age, address });
+  i++;
+}
+
+console.log("While loop result:", resultWhile);
